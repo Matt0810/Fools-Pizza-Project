5dfdee22eafc2c5a8a8f124e4bcae51b49f5628f
Matthew Enriquez <matthewenriquez06@live.com>
2023-12-14 16:12:34 -0500

    added first modal

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 import config from '../config/index.json';
 
 const About = () => {
@@ -7,86 +7,121 @@ const About = () => {
   const { logo, name: companyName } = company;
   const { socialMedia, sections } = about;
 
+  // State for managing modal open/close
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Function to open the modal
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Modal styles to customize its appearance
+  const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      maxWidth: '1200px', // Adjust the maximum width as needed
+      width: '100%',
+    },
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+  };
+
+  // Effect to manage body overflow based on modal status
+  useEffect(() => {
+    document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
+
+    // Cleanup function to restore body overflow when the component is unmounted
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isModalOpen]);
+
   return (
-    <div
-      id="about"
-      className="mx-auto container xl:px-20 lg:px-12 sm:px-6 px-4 py-12"
-    >
+    <div id="about" className="mx-auto container xl:px-20 lg:px-12 sm:px-6 px-4 py-12">
       <div className="flex flex-col items-center justify-center">
         <div>
           <img src={logo} alt={companyName} className="w-16 h-16" />
         </div>
         <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-4 h-12">
           {sections.map((section, index) => (
-            <a
+            <span
               key={`${section.name}-${index}`}
-              href={section.href}
+              onClick={openModal}
               className="hover:text-primary text-base cursor-pointer leading-4 text-gray-800 dark:text-gray-400 dark:hover:text-white"
             >
               {section.name}
-            </a>
+            </span>
           ))}
         </div>
-        <div className="flex items-center gap-x-8 mt-6 h-8">
-          <a
-            aria-label="github"
-            href={socialMedia.github}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              className="fill-current text-gray-800 dark:text-white hover:text-primary"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-            </svg>
-          </a>
-          <a
-            aria-label="linkedin"
-            href={socialMedia.linkedin}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              className="fill-current text-gray-800 dark:text-white hover:text-primary"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-            </svg>
-          </a>
-          <a
-            aria-label="twitter"
-            href={socialMedia.twitter}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              className="fill-current text-gray-800 dark:text-white hover:text-primary"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
-            </svg>
-          </a>
-        </div>
-        <div className="flex items-center mt-6">
-          <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
-            &copy; {new Date().getFullYear()} designed by{' '}
-            <a href="https://github.com/issaafalkattan" rel="nofollow">
-              Issaaf Kattan
-            </a>
-          </p>
-        </div>
       </div>
+
+      {/* Modal component */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Section Modal"
+        style={modalStyles} // Apply the custom styles
+      >
+<div>
+  <h1>Privacy Policy</h1>
+  <p><strong>1. Introduction</strong></p>
+  <p>[Fool's Pizza] ("we," "us," or "our") is committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, and safeguard your data, especially when using Google Analytics.</p>
+
+  <p><strong>2. What is Google Analytics?</strong></p>
+  <p>Google Analytics is a web analytics service provided by Google Inc. ("Google"). It uses cookies and other technologies to collect and analyze information about the use of our website. This information is used to generate reports and statistics to help us understand how visitors interact with our site.</p>
+
+  <p><strong>3. Data Collected by Google Analytics</strong></p>
+  <p>Google Analytics may collect various types of data, including:</p>
+  <ul>
+    <li>IP addresses</li>
+    <li>Browser types</li>
+    <li>Operating systems</li>
+    <li>Device types</li>
+    <li>Geographic locations</li>
+    <li>Page views and interactions</li>
+  </ul>
+  <p>This data is collected anonymously and does not personally identify you. However, it may be considered personal data under the GDPR.</p>
+
+  <p><strong>4. Legal Basis for Processing</strong></p>
+  <p>The legal basis for processing personal data through the use of Google Analytics is our legitimate interest in understanding how visitors interact with our website and improving its functionality and content. We ensure that the data collected is used in a manner consistent with the purposes outlined in this Privacy Policy.</p>
+
+  <p><strong>5. Your Consent</strong></p>
+  <p>By using our website, you consent to the processing of your data by Google Analytics as described in this Privacy Policy.</p>
+
+  <p><strong>6. Google's Data Processing Amendment</strong></p>
+  <p>To comply with the GDPR, we have entered into a Data Processing Amendment (DPA) with Google. The DPA includes Google's commitment to process data in accordance with the GDPR requirements.</p>
+
+  <p><strong>7. Your Rights under the GDPR</strong></p>
+  <p>You have the right to:</p>
+  <ul>
+    <li>Access your personal data</li>
+    <li>Rectify inaccuracies in your personal data</li>
+    <li>Erase your personal data</li>
+    <li>Restrict processing of your personal data</li>
+    <li>Object to the processing of your personal data</li>
+    <li>Receive your personal data in a structured, commonly used, and machine-readable format</li>
+  </ul>
+  <p>To exercise these rights or for any questions regarding our use of Google Analytics and compliance with the GDPR, please contact us at [your contact email/address].</p>
+
+  <p><strong>8. Changes to this Policy</strong></p>
+  <p>We reserve the right to update or modify this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. Any such changes will be effective immediately upon posting the updated Privacy Policy on our website.</p>
+</div>
+
+        <button onClick={closeModal}>Accept Policy</button>
+      </Modal>
     </div>
   );
 };
+
 export default About;
