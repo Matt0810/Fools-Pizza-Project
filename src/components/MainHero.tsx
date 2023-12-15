e31a97d50c62f01f434d2a7300951baa95bcb8c0
Matthew Enriquez <matthewenriquez06@live.com>
2023-12-15 14:40:09 -0500

    button now takes you to sign up section

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-scroll'; // Import Link from react-scroll
 
 import config from '../config/index.json';
 
 const MainHero = () => {
   const { mainHero } = config;
+
   return (
     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div className="sm:text-center lg:text-left">
@@ -18,12 +20,16 @@ const MainHero = () => {
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
-            <a
-              href={mainHero.primaryAction.href}
+            <Link
+              spy={true}
+              activeClass="active" 
+              to="features" 
+              smooth={true}
+              duration={1000}
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
             >
               {mainHero.primaryAction.text}
-            </a>
+            </Link>
           </div>
           <div className="mt-3 sm:mt-0 sm:ml-3">
             <a
