1965e315ec08928112893a2f2f2c57805e2d030f
Matthew Enriquez <matthewenriquez06@live.com>
2023-12-15 18:31:43 -0500

    fixed modal and added first playwright test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,20 +7,16 @@ const About = () => {
   const { logo, name: companyName } = company;
   const { sections } = about;
 
-  // State for managing modal open/close
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // Function to open the modal
   const openModal = () => {
     setIsModalOpen(true);
   };
 
-  // Function to close the modal
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  // Modal styles to customize its appearance
   const modalStyles = {
     content: {
       top: '50%',
@@ -29,7 +25,7 @@ const About = () => {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
-      maxWidth: '1200px', // Adjust the maximum width as needed
+      maxWidth: '1200px',
       width: '100%',
     },
     overlay: {
@@ -37,15 +33,23 @@ const About = () => {
     },
   };
 
-  // Effect to manage body overflow based on modal status
   useEffect(() => {
     document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
 
-    // Cleanup function to restore body overflow when the component is unmounted
     return () => {
       document.body.style.overflow = 'auto';
     };
   }, [isModalOpen]);
+
+  const isLocalStorageAvailable = () => {
+    try {
+      localStorage.setItem('test', 'test');
+      localStorage.removeItem('test');
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
 
   return (
     <div id="about" className="mx-auto container xl:px-20 lg:px-12 sm:px-6 px-4 py-12">
@@ -66,59 +70,79 @@ const About = () => {
         </div>
       </div>
 
-      {/* Modal component */}
+      {isLocalStorageAvailable() && !localStorage.getItem('cookiePolicyAccepted') && (
+        <div className="fixed bottom-0 left-0 right-0 p-4 bg-gray-800 text-white text-center">
+          <p>
+            We use cookies to improve your experience. By continuing to browse, you accept our{' '}
+            <button onClick={openModal} className="underline focus:outline-none">
+              cookie policy
+            </button>
+          </p>
+        </div>
+      )}
+
       <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         contentLabel="Section Modal"
-        style={modalStyles} // Apply the custom styles
+        style={modalStyles}
       >
-<div>
-  <h1>Privacy Policy</h1>
-  <p><strong>1. Introduction</strong></p>
-  <p>[Fool's Pizza] ("we," "us," or "our") is committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, and safeguard your data, especially when using Google Analytics.</p>
+        <div>
+          {/* Privacy Policy content */}
+          <h1>Privacy Policy Section: Google Analytics and GDPR Compliance</h1>
+          <p>1. Introduction</p>
+          <p>[Fool's Pizza] ("we," "us," or "our") is committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, and safeguard your data, especially when using Google Analytics.</p>
 
-  <p><strong>2. What is Google Analytics?</strong></p>
-  <p>Google Analytics is a web analytics service provided by Google Inc. ("Google"). It uses cookies and other technologies to collect and analyze information about the use of our website. This information is used to generate reports and statistics to help us understand how visitors interact with our site.</p>
+          <p>2. What is Google Analytics?</p>
+          <p>Google Analytics is a web analytics service provided by Google Inc. ("Google"). It uses cookies and other technologies to collect and analyze information about the use of our website. This information is used to generate reports and statistics to help us understand how visitors interact with our site.</p>
 
-  <p><strong>3. Data Collected by Google Analytics</strong></p>
-  <p>Google Analytics may collect various types of data, including:</p>
-  <ul>
-    <li>IP addresses</li>
-    <li>Browser types</li>
-    <li>Operating systems</li>
-    <li>Device types</li>
-    <li>Geographic locations</li>
-    <li>Page views and interactions</li>
-  </ul>
-  <p>This data is collected anonymously and does not personally identify you. However, it may be considered personal data under the GDPR.</p>
+          <p>3. Data Collected by Google Analytics</p>
+          <p>Google Analytics may collect various types of data, including:</p>
+          <ul>
+            <li>IP addresses</li>
+            <li>Browser types</li>
+            <li>Operating systems</li>
+            <li>Device types</li>
+            <li>Geographic locations</li>
+            <li>Page views and interactions</li>
+          </ul>
+          <p>This data is collected anonymously and does not personally identify you. However, it may be considered personal data under the GDPR.</p>
 
-  <p><strong>4. Legal Basis for Processing</strong></p>
-  <p>The legal basis for processing personal data through the use of Google Analytics is our legitimate interest in understanding how visitors interact with our website and improving its functionality and content. We ensure that the data collected is used in a manner consistent with the purposes outlined in this Privacy Policy.</p>
+          <p>4. Legal Basis for Processing</p>
+          <p>The legal basis for processing personal data through the use of Google Analytics is our legitimate interest in understanding how visitors interact with our website and improving its functionality and content. We ensure that the data collected is used in a manner consistent with the purposes outlined in this Privacy Policy.</p>
 
-  <p><strong>5. Your Consent</strong></p>
-  <p>By using our website, you consent to the processing of your data by Google Analytics as described in this Privacy Policy.</p>
+          <p>5. Your Consent</p>
+          <p>By using our website, you consent to the processing of your data by Google Analytics as described in this Privacy Policy.</p>
 
-  <p><strong>6. Google's Data Processing Amendment</strong></p>
-  <p>To comply with the GDPR, we have entered into a Data Processing Amendment (DPA) with Google. The DPA includes Google's commitment to process data in accordance with the GDPR requirements.</p>
+          <p>6. Google's Data Processing Amendment</p>
+          <p>To comply with the GDPR, we have entered into a Data Processing Amendment (DPA) with Google. The DPA includes Google's commitment to process data in accordance with the GDPR requirements.</p>
 
-  <p><strong>7. Your Rights under the GDPR</strong></p>
-  <p>You have the right to:</p>
-  <ul>
-    <li>Access your personal data</li>
-    <li>Rectify inaccuracies in your personal data</li>
-    <li>Erase your personal data</li>
-    <li>Restrict processing of your personal data</li>
-    <li>Object to the processing of your personal data</li>
-    <li>Receive your personal data in a structured, commonly used, and machine-readable format</li>
-  </ul>
-  <p>To exercise these rights or for any questions regarding our use of Google Analytics and compliance with the GDPR, please contact us at [your contact email/address].</p>
+          <p>7. Your Rights under the GDPR</p>
+          <p>You have the right to:</p>
+          <ul>
+            <li>Access your personal data</li>
+            <li>Rectify inaccuracies in your personal data</li>
+            <li>Erase your personal data</li>
+            <li>Restrict processing of your personal data</li>
+            <li>Object to the processing of your personal data</li>
+            <li>Receive your personal data in a structured, commonly used, and machine-readable format</li>
+          </ul>
+          <p>To exercise these rights or for any questions regarding our use of Google Analytics and compliance with the GDPR, please contact us at [your contact email/address].</p>
 
-  <p><strong>8. Changes to this Policy</strong></p>
-  <p>We reserve the right to update or modify this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. Any such changes will be effective immediately upon posting the updated Privacy Policy on our website.</p>
-</div>
+          <p>8. Changes to this Policy</p>
+          <p>We reserve the right to update or modify this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. Any such changes will be effective immediately upon posting the updated Privacy Policy on our website.</p>
+        </div>
 
-        <button onClick={closeModal}>Accept Policy</button>
+        <button
+          onClick={() => {
+            if (isLocalStorageAvailable()) {
+              localStorage.setItem('cookiePolicyAccepted', 'true');
+            }
+            closeModal();
+          }}
+        >
+          Accept Policy
+        </button>
       </Modal>
     </div>
   );
