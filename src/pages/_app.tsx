cd97652487c2adeddf5a0dd02014b192d979a8b3
Matthew Enriquez <matthewenriquez06@live.com>
2023-12-15 13:53:43 -0500

    added javascript code for analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
-import { AppProps } from 'next/app';
 
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QRDCN3V411"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QRDCN3V411');
+        `}
+      </script>
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
