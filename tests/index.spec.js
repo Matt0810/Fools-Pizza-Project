832dac4781be3fde18371687eb1494d701f1b4b0
Matthew Enriquez <matthewenriquez06@live.com>
2023-12-17 14:44:17 -0500

    added test for hero section text

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -71,3 +71,19 @@ test("index.json contains 'Privacy'", async ({ page }) => {
   // Check if the content contains the text "Privacy"
   expect(jsonContent).toHaveProperty("text", "Privacy");
 });
+
+test("index.json contains newsletter message", async ({ page }) => {
+  // Load the index.json file
+  await page.goto(`${localhost}/index.json`);
+
+  // Get the content of the index.json file
+  const content = await page.textContent("pre");
+
+  // Parse the content as JSON
+  const jsonContent = JSON.parse(content);
+
+  // Check if the content contains the specified newsletter message
+  const expectedMessage =
+    "Sign Up To Fool's Pizza Newsletter To Stay Up To Date And Receive 30% Off Your Next Order!";
+  expect(jsonContent).toHaveProperty("newsletterMessage", expectedMessage);
+});
