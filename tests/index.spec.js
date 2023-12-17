e001e6e9fc6f904e3cb37050b685fe0400bcf913
Matthew Enriquez <matthewenriquez06@live.com>
2023-12-17 14:41:34 -0500

    added test for reviews

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -43,3 +43,17 @@ test("index.json contains 'Newsletter'", async ({ page }) => {
   // Check if the content contains the text "Newsletter"
   expect(jsonContent).toHaveProperty("text", "Newsletter");
 });
+
+test("index.json contains 'Reviews'", async ({ page }) => {
+  // Load the index.json file
+  await page.goto(`${localhost}/index.json`);
+
+  // Get the content of the index.json file
+  const content = await page.textContent("pre");
+
+  // Parse the content as JSON
+  const jsonContent = JSON.parse(content);
+
+  // Check if the content contains the text "Reviews"
+  expect(jsonContent).toHaveProperty("text", "Reviews");
+});
